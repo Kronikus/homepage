c06353495b97d4c2c92becf331fab6261276ea3a
kronikus <andrew@houseofkim.com>
2017-12-26 19:29:20 -0500

    Init under construction

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+
+        <router-link to="/" exact>
+          <img class="logo" src="./assets/logo.png">
+        </router-link>
+        <router-link to="/about">About</router-link>
+        <router-link to="/services">Services</router-link>
+        <router-link to="/contact">Contact</router-link>
+        <transition name="fade" mode="out-in">
+          <router-view class="view"> </router-view>
+        </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-color:aquamarine;
+  height: 57px;
+  left: 0px;
+  right: 0px;
+  top:0px;
+  position:fixed;
+  z-index:999;
+
+}
+.logo {
+      height: 56px;
+      margin-right:10px;
+      display: inline-block;
+      vertical-align:middle;
+      };
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import About from '@/components/About'
+import Services from '@/components/Services'
+import Contact from '@/components/Contact'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About
+    },
+    {
+      path: '/services',
+      name: 'Services',
+      component: Services
+    },
+    {
+      path: '/contact',
+      name: 'Contact',
+      component: Contact
+    }
+  ]
+})
