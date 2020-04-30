1125ed2b75b6d1020c2348b7082c0bb2f14858a9
kronikus <andrew@houseofkim.info>
2020-04-30 08:38:32 -0400

    adding video

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@
         <transition name="fade" mode="out-in">
           <router-view class="view"> </router-view>
         </transition>
+       
   </div>
 </template>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,21 @@
 <template>
+ <div> <!-- root node -->
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <a> <img class="pic" src="../assets/cat.jpeg"> </a>
+    <p> <img class="pic" src="../assets/cat.jpeg"> </p>
+    <table class="table">
+        <th>Video</th>
+        <th>Chat</th>
+        
+        <tr>
+        <td><iframe src="https://player.twitch.tv/?channel=l1ve1nthesky" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe><a href="https://www.twitch.tv/l1ve1nthesky?tt_content=text_link&tt_medium=live_embed" style="padding:2px 0px 4px; display:block; width:345px; font-weight:normal; font-size:10px; text-decoration:underline;">Watch live video from L1ve1nTheSky on www.twitch.tv</a></td>
+        <td><iframe src="https://www.twitch.tv/embed/l1ve1nthesky/chat" frameborder="0" scrolling="no" height="500" width="350"></iframe></td>
+        </tr>
+        <td>Data 3</td>
+        
+    </table>
   </div>
+ </div>
 </template>
 
 <script>
